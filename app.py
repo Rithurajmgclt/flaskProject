b4670dfbb1f49bfc12fbb209906325ce846b9976
Rithurajmgclt <rithurajmg2015@gmail.com>
2020-08-07 13:28:22 +0530

    pageinatin in useradd

--- app.py
+++ app.py
@@ -16,29 +16,33 @@ app.run(debug=True)
 
 @app.route('/')
 def index():
-    current_page= int(request.args.get('page'))-1
-    print(current_page)
+    if request.args.get('page')!=None:
+
+        current_page= int(request.args.get('page'))-1
+    else:
+        current_page=0
+
     sql="SELECT * FROM users_table order by user_id desc limit %s,2"
-    values=(current_page,)
+    values = (current_page,)
     newcursor.execute(sql,values)
-    details=newcursor.fetchall()
+    details = newcursor.fetchall()
     newcursor.execute("SELECT count(*) total_rec FROM maindb.users_table")
-    page=newcursor.fetchone()
+    page = newcursor.fetchone()
     total_pages=int(page['total_rec']/2)  # no of items per page
-    return render_template("index.html",details=details,total_pages=total_pages)
+    return render_template("index.html",details=details,total_pages=total_pages,current_page=current_page)
 
 @app.route('/add_details',methods=['GET','POST'])
 def add_details():
 
-    if request.method=='POST':
-        firstname=escape (request.form['FirstName'])
-        lastname=escape(request.form['LastName'])
-        age=escape(request.form['Age']) 
-        email=escape (request.form['Email'])
-        phonenumber=escape(request.form['PhoneNumber'])
-        address=escape(request.form['Address'])
-        userroles=escape(request.form['UserRoles'])
-        password=escape(request.form['Password'])
+    if request.method == 'POST':
+        firstname = escape (request.form['FirstName'])
+        lastname = escape(request.form['LastName'])
+        age = escape(request.form['Age']) 
+        email = escape (request.form['Email'])
+        phonenumber = escape(request.form['PhoneNumber'])
+        address = escape(request.form['Address'])
+        userroles = escape(request.form['UserRoles'])
+        password = escape(request.form['Password'])
 
 
         sql=""" INSERT INTO users_table(firstname,lastname,age,email,phonenumber,address,userroles,password)
@@ -51,6 +55,5 @@ def add_details():
         return redirect(url_for('index'))
 
     return render_template('submit.html')
-
 
 
